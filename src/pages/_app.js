4843dfe6624991d027ddbe40614bba0ce7ba67d1
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-02-27 16:38:57 +0000

    pages

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,12 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps, router }) {
-  const isHomePage = router.pathname === '/';
+  const isHomePage = router.pathname === '/foodies';
+  const isLocalHost = router.pathname === '/';
   const isSignUp = router.pathname === '/signup';
   const islogin = router.pathname === '/login'
 
-  if (isHomePage || isSignUp || islogin) {
+  if (isHomePage || isSignUp || islogin || isLocalHost) {
     return <Component {...pageProps} />
   }
 
