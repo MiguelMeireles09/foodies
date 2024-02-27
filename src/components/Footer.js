76b991d8d5fb377962b07071d2d64d2ce89ee5e7
MiguelMeireles09 <miguelmeireles025@gmail.com>
2024-02-27 16:27:33 +0000

    img

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,12 @@
+import Image from "next/image"
+
 export default function Footer() {
     return (
         <footer className="bg-black text-white fixed w-full bottom-0">
             <p>
             <span>Nome Projeto</span>
             &copy; 2024</p>
+            <Image src="/heart.png" width="40" height="40" />
         </footer>
     )
 }
