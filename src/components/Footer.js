d038002a3ca8152d9a1bdd3d6ad6a1ad6076a82d
Miguel <miguelmeireles025@gmail.com>
2024-02-27 21:18:20 +0000

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="grid justify-center fixed w-full bottom-0 flex-col">
-      <div className="col-span-3">
-        <Image src="/row.png" width="500" height="2" />
-      </div>
-      <div className="flex gap-11 justify-center footer pt-2">
+    <footer className="grid justify-center fixed w-full bottom-0">
+      <div className="flex gap-11 justify-center footer pt-2 border-grey ">
         <Image src="/logofooter.png" width="60" height="60" />
         <Image src="/heart.png" width="50" height="50" />
         <Image src="/person1.png" width="50" height="50" />
