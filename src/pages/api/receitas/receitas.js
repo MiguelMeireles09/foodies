04108c48d33b9ccdcb948308fcb0c692d165d98f
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-02-27 15:30:56 +0000

    receitas

--- src/pages/api/receitas/receitas.js
+++ src/pages/api/receitas/receitas.js
@@ -5,7 +5,6 @@ export default async function handler(req, res) {
     try {
         if (req.method === "POST") {
             const { titulo } = req.body;
-            console.log(titulo)
             const receita = await procurarReceita(titulo);
             if (receita) {
                 return res.status(200).json(receita);
