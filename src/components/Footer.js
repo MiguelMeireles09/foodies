594ef2dd6b8ab074f717238814f46407872fc934
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-02-28 17:23:57 +0000

    footer svgs

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,13 +6,16 @@ export default function Footer() {
     <footer className="fixed bottom-0 w-full bg-white">
       <div className="flex justify-around items-center border-t-2 border-gray-300 py-2">
         <Link href="/foodies/homepage">
+          <Image src="/footer/PanelaVazia.svg" width="40" height="40" />
           <Image src="/footer/PanelaCheia.svg" width="40" height="40" />
         </Link>
         <Link href="/foodies/search">
           <Image src="/footer/search.svg" width="36" height="36" />
+          <Image src="/footer/searching.svg" width="36" height="36" />
         </Link>
         <Link href="/foodies/favoritos">
           <Image src="/footer/heart.png" width="40" height="40" />
+          <Image src="/footer/Heartfull.svg" width="40" height="40" />
         </Link>
         <Link href="/foodies/perfil">
           <Image src="/footer/person1.png" width="40" height="40" />
