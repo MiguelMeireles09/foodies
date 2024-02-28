121246bcc428644e652c3a6b0869a44e6b3d1c97
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-02-28 12:10:05 +0000

    fixed tailwidn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: {
-    enabled: true,
-    content: [
-      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-      extend: {
-        backgroundImage: {
-          "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-          "gradient-conic":
-            "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        },
-        colors: {
-          grey: "#868686",
-        },
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         cinza: "#868686",
         cinzaClaro: "#D9D9D9",
         verde: "#306754", 
-        botãoInicial: "#252525", 
-        cinzaBorder: "#979797"
+        butãoInicial: "#252525"
       }
     },
-
-    plugins: [],
   },
+  plugins: [],
 };
