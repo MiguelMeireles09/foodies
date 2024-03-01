4cd1e942597e9a69f8f6469ad6f99fa5bfdacf20
MariaBeatrizCarneiro <up202107798@up.pt>
2024-03-01 10:46:20 +0000

    FOODIES svg atualizado

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ export default function Header() {
     return (
         <nav className="bg-white border-b-2 text-black flex pl-12 md:pl-20 lg:pl-28 xl:pl-40 sticky top-0 z-50">
             <div>
-                <Image src="/images/FOODIES.svg" className="pt-6 pb-4" width="202" height="40" />
+                <Image src="/images/FOODIES.svg" className="pt-6 pb-4" width="202" height="40" alt="FOODIES"/>
             </div>
         </nav>
     );
