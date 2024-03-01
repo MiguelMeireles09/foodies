d6ba36f5b32d0b86834734df75dc888d3b61a5f4
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-03-01 10:59:34 +0000

    fixed

--- src/pages/data/verificaToken.js
+++ src/pages/data/verificaToken.js
@@ -14,7 +14,7 @@ async function verificaTokenLogado(token) {
 const collectionNameSignUp = "signupform"
 async function dadosAtravesToken(pegaId){
     const collection = await getMongoCollection(collectionNameSignUp)
-    const result = await collection.findOne({_id : {$eq: ObjectId(pegaId)}})
+    const result = await collection.findOne({_id : {$eq: new ObjectId(pegaId)}})
     return result
 }
 
