ad545edaf1ba9c299745a6a773fb6dca5c1899e7
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-03-02 22:48:42 +0000

    fixed

--- src/pages/foodies/perfil.js
+++ src/pages/foodies/perfil.js
@@ -20,20 +20,32 @@ export default function PerfilPage() {
   }
 
   return (
-    <div>
-      <div className="flex justify-center perfilImagem">
-        <Image
-          src="/images/BottomBackgroundFoodies.png"
-          alt="Perfil Imagem"
-          className="rounded-full"
-          width={200}
-          height={200}
-        />
+    <div className="flex flex-col  min-h-screen pt-12">
+      <div className="relative mb-4 pb-16">
+        <div className=" flex justify-center">
+          <Image
+            src="/images/chapeuPerfil.svg"
+            alt="Chapeu Chef"
+            className="rounded-full"
+            width={200}
+            height={200}
+          />
+        </div>
+        <div className="flex justify-center">
+          <Image
+            src="/images/BottomBackgroundFoodies.png"
+            alt="Perfil Imagem"
+            className="rounded-full"
+            width={350}
+            height={400}
+          />
+        </div>
       </div>
-      <div className="w-40 flex flex-col align-middle justify-center">
-      <h1 className="justify-center flex">Nome Utilizador: {userData.nomeUsuario}</h1>
-      <h1 className="justify-center flex">Email do Utilizador: {userData.email}</h1>
-      <button className=" bg-verde rounded-xl p-4 " onClick={handleLogout}>Terminar Sessão</button>
+      <div className="w-full flex flex-col items-center justify-center ">
+        <h1>Nome Utilizador:</h1>
+        <h1>{userData.nomeUsuario}</h1>
+        <h1>Email do Utilizador:</h1><h1> {userData.email}</h1>
+        <button className="bg-verde rounded-xl p-4 mt-32" onClick={handleLogout}>Terminar Sessão</button>
       </div>
     </div>
   );
