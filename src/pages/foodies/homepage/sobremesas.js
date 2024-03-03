2dea88b2df8191ec1132c6b2641a08e9be9e9be2
Fabio Silva <121696024+FabioSilva-code@users.noreply.github.com>
2024-03-03 15:56:51 +0000

    trying to do carrousel

--- src/pages/foodies/homepage/sobremesas.js
+++ src/pages/foodies/homepage/sobremesas.js
@@ -3,7 +3,7 @@ import { useState, useEffect } from 'react';
 
 export default function Sobremesa() {
   const [receitas, setReceitas] = useState([]);
-  const [currentIndex, setCurrentIndex] = useState(0);
+
 
   const fetchReceitas = async () => {
     try {
@@ -18,34 +18,25 @@ export default function Sobremesa() {
     }
   };
   
+
   useEffect(() => {
     fetchReceitas();
   }, []);
-  
-  const goToPrevious = () => {
-    const isFirstRecipe = currentIndex === 0;
-    const newIndex = isFirstRecipe ? receitas.length - 1 : currentIndex - 1;
-    setCurrentIndex(newIndex);
-  };
-  
-  const goToNext = () => {
-    const isLastRecipe = currentIndex === receitas.length - 1;
-    const newIndex = isLastRecipe ? 0 : currentIndex + 1;
-    setCurrentIndex(newIndex);
-  };
+
 
   return (
     <div className="min-h-screen">
       <p className='text-3xl text-center py-5'>Receitas Baratas:</p>
-      {receitas.length > 0 && (
-        <div>
-          <button onClick={goToPrevious}>Previous</button>
-          <div>
-            <img src={receitas[currentIndex].fotoReceita} alt={receitas[currentIndex].titulo} style={{ width: '100%', height: 'auto' }} />
-            <p className='text-2xl py-2 text-center'>{receitas[currentIndex].titulo}</p>
-          </div>
-          <button onClick={goToNext}>Next</button>
-        </div>
-      )}
+       <ul>
+         {receitas.map(e => {
+          return (
+            <li>
+              <img src={e.fotoReceita}/>
+              <p className='text-2xl py-2'>{e.titulo}</p>
+            </li>
+          );
+        })}
+      </ul>
     </div>
-  )}
+  );
+}
--- src/pages/foodies/homepage/top10.js
+++ src/pages/foodies/homepage/top10.js
@@ -1,9 +1,9 @@
-// pages/foodies/homepageCards/top10.js
-
+import { Carousel } from "@material-tailwind/react";
 import { useState, useEffect } from 'react';
 
 export default function Top10() {
   const [receitas, setReceitas] = useState([]);
+  const [page, setPage] = useState(0);
 
   const fetchReceitas = async () => {
     try {
@@ -21,20 +21,32 @@ export default function Top10() {
   useEffect(() => {
     fetchReceitas();
   }, []);
-  
+
+  const goToPrevious = () => {
+    setPage((prevPage) => (prevPage > 0 ? prevPage - 1 : receitas.length - 1));
+  };
+
+  const goToNext = () => {
+    setPage((prevPage) => (prevPage + 1) % receitas.length);
+  };
+
   return (
-    <div className="min-h-screen">
-      <p className='text-3xl text-center py-5'>Receitas Baratas:</p>
-       <ul>
-         {receitas.map(e => {
-          return (
-            <li>
-              <img src={e.fotoReceita}/>
-              <p className='text-2xl py-2'>{e.titulo}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <main className="relative flex h-[calc(100vh-70px)] w-screen flex-col justify-center items-center">
+      {receitas.length > 0 ? (
+        <>
+          <button onClick={goToPrevious} className="absolute left-0 z-10">Prev</button>
+          <div className="w-full p-10">
+            <img src={receitas[page].fotoReceita} className="w-full h-full object-cover object-center" alt={receitas[page].titulo}/>
+            <div className="absolute bottom-8 left-10 py-3 px-6 bg-white rounded-lg">
+              <h2 className="text-4xl">{receitas[page].titulo}</h2>
+              <p className="text-2xl mt-4 text-verde">Magia</p>
+            </div>
+          </div>
+          <button onClick={goToNext} className="absolute right-0 z-10">Next</button>
+        </>
+      ) : (
+        <p>Carregando receitas...</p>
+      )}
+    </main>
   );
 }
