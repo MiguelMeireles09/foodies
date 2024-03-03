1e0a216481ace78d79ad2313bfd559364143f6d1
Miguel <miguelmeireles025@gmail.com>
2024-03-03 15:54:18 +0000

    funcionalidade dos favoritos para a receita

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,25 +9,25 @@ export default function Footer() {
     <footer className="fixed bottom-0 w-full bg-white">
       <div className="flex justify-around items-center border-t-2 border-gray-300 py-2">
         <Link href="/foodies/homepage">
-        {router.route === "/foodies/homepage" ? (
-          <Image src="/footer/PanelaCheia.svg" width="40" height="40" />
-        ) : (
-          <Image src="/footer/PanelaVazia.svg" width="40" height="40" />
-        )}
+          {router.route === "/foodies/homepage" ? (
+            <Image src="/footer/PanelaCheia.svg" width="40" height="40" />
+          ) : (
+            <Image src="/footer/PanelaVazia.svg" width="40" height="40" />
+          )}
         </Link>
         <Link href="/foodies/search">
           {router.route === "/foodies/search" ? (
-          <Image src="/footer/searching.svg" width="36" height="36" />
-          ):(
-          <Image src="/footer/search.svg" width="36" height="36" />
+            <Image src="/footer/searching.svg" width="36" height="36" />
+          ) : (
+            <Image src="/footer/search.svg" width="36" height="36" />
           )}
         </Link>
         <Link href="/foodies/favoritos">
-        {router.route === "/foodies/favoritos" ? (
-          <Image src="/footer/Heartfull.svg" width="40" height="40" />
-        ):(
-          <Image src="/footer/heart.png" width="40" height="40" />
-        )}
+          {router.route === "/foodies/favoritos" ? (
+            <Image src="/footer/Heartfull.svg" width="40" height="40" />
+          ) : (
+            <Image src="/footer/heart.png" width="40" height="40" />
+          )}
         </Link>
         <Link href="/foodies/perfil">
           <Image src="/footer/person1.png" width="40" height="40" />
--- src/pages/foodies/favoritos.js
+++ src/pages/foodies/favoritos.js
@@ -3,6 +3,7 @@ import CardFavoritos from "@/components/CardFavoritos";
 import ProtectPage from "@/utils/hooks/protectPagesHook";
 import ReceitaInfo from './receita';
 import Link from 'next/link';
+import { router } from 'next/router';
 
 export default function FavoritosPage() {
   const { loading, userData } = ProtectPage();
@@ -35,6 +36,16 @@ export default function FavoritosPage() {
     }
   };
 
+  const handleImagemClick = (recipe) => {
+    const receitaSelecionada = recipe.titulo
+    console.log("Receita clicada:", receitaSelecionada);
+    router.push({
+      pathname: '/foodies/receita',
+      query: { query: receitaSelecionada }
+    });
+  }
+  
+
   if (loading) return <div>Loading...</div>;
 
   return (
@@ -43,12 +54,14 @@ export default function FavoritosPage() {
 
         {/* mesmo card do search , fica meio estragado quando apenas temos uma receita */}
         <p className="text-center py-5 text-2xl 2xl:text-4xl">Os teus favoritos:</p>
-        <Link href="/foodies/receita">Receita</Link>
+        {favoritos.length === 0 && (
+        <div>Ainda não tens nenhuma receita adicionada aos teus favoritos. <Link href={"/foodies/search"}>Adiciona aqui :)</Link></div>
+      )}
         <div className="flex flex-wrap mb-10 pb-10">
         {favoritos.map((recipe) => ( // Changed 'e' to 'recipe' for clarity
           <div key={recipe._id} className="w-1/2 md:w-1/3 lg:w-1/4 p-4"> {/* Ensure recipe._id is the correct key */}
-            <div className="bg-cinzaClaro rounded-2xl h-full flex flex-col justify-between">
-              <img src={recipe.fotoReceita} alt="Favorite Recipe" className="rounded-t-2xl w-full h-40 object-cover" /> {/* Added alt attribute for accessibility */}
+            <div onClick={() => handleImagemClick(recipe)} className="bg-cinzaClaro rounded-2xl h-full flex flex-col justify-between">
+              <img  src={recipe.fotoReceita} alt="Favorite Recipe" className="rounded-t-2xl w-full h-40 object-cover" /> {/* Added alt attribute for accessibility */}
               <div className="flex-grow flex flex-col justify-center border-t-2 border-cinza">
                 <p className="font-sans font-normal text-center p-3 text-sm md:text-base lg:text-lg xl:text-xl text-black">{recipe.titulo}</p>
               </div>
