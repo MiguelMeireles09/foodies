fae1cbd9636f549805bc4641da121ed75c142cf6
Miguel <miguelmeireles025@gmail.com>
2024-03-07 21:43:18 +0000

    tentativa implementacao do perfil avatar change

--- src/components/profile/ProfileImage.jsx
+++ src/components/profile/ProfileImage.jsx
@@ -61,3 +61,64 @@ const UserProfile = () => {
 };
 
 export default UserProfile;
+
+
+
+
+// import React, { useState, useRef } from 'react';
+// import Avatar from 'react-avatar';
+
+// const UserProfile = () => {
+//   const [avatarUrl, setAvatarUrl] = useState('');
+//   const fileInputRef = useRef(null);
+
+//   const handleAvatarChange = (newAvatar) => {
+//     setAvatarUrl(newAvatar);
+//   };
+
+//   const handleFileInputChange = async (event) => {
+//     const file = event.target.files[0];
+//     if (file) {
+//       try {
+//         const formData = new FormData();
+//         formData.append('avatar', file);
+
+//         const response = await fetch('/api/user/upload', {
+//           method: 'POST',
+//           body: formData
+//         });
+
+//         if (response.ok) {
+//           const data = await response.json();
+//           console.log(data.imageUrl)
+//           handleAvatarChange(data.imageUrl);
+//         } else {
+//           console.error('Error uploading image:', response.statusText);
+//         }
+//       } catch (error) {
+//         console.error('Error uploading image:', error);
+//       }
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <input
+//         type="file"
+//         accept="image/*"
+//         onChange={handleFileInputChange}
+//         style={{ display: 'none' }}
+//         ref={fileInputRef}
+//       />
+//       <Avatar
+//         src={avatarUrl}
+//         name="User Name"
+//         size="200"
+//         round
+//         onClick={() => fileInputRef.current.click()}
+//       />
+//     </div>
+//   );
+// };
+
+// export default UserProfile;
